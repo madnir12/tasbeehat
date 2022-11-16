386231c3be745ac11e71bf99ddba3167fa8ec104
madnir12 <anfz12001@gmail.com>
2022-11-16 21:12:51 +0500

    Updates

---
+++ precache-manifest.bb95656aa7b46b4184bdf0dd684cc525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2dc7cd86a08156fb8f",
+    "url": "/tasbeehat/static/css/main.2cca9e81.chunk.css"
+  },
+  {
+    "revision": "ce2dc7cd86a08156fb8f",
+    "url": "/tasbeehat/static/js/main.ce2dc7cd.chunk.js"
+  },
+  {
+    "revision": "6392ae813b56f037cdef",
+    "url": "/tasbeehat/static/js/1.6392ae81.chunk.js"
+  },
+  {
+    "revision": "d42344ea9b13bcb705a2",
+    "url": "/tasbeehat/static/js/runtime~main.d42344ea.js"
+  },
+  {
+    "revision": "1926cd424cf97f9bc7c01aee89e5e7b9",
+    "url": "/tasbeehat/static/media/noorehuda Regular.1926cd42.ttf"
+  },
+  {
+    "revision": "642504988927ac898a3b4cff1357d0b0",
+    "url": "/tasbeehat/static/media/Jameel Noori Nastaleeq 2.64250498.ttf"
+  },
+  {
+    "revision": "6522cfff84c2cdf85dff59c24a725f67",
+    "url": "/tasbeehat/index.html"
+  }
+];
